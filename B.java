3534027d1f73bef1fb7556729a2bbb06e2d7795e
Jeremaihyang <1850822729@qq.com>
2018-11-06 23:45:16 +0800

    update B.java

--- B.java
+++ B.java
@@ -1,6 +1,10 @@
 public class A{
 	public static void main(){
 		System.out.println("hello world"):
+		System.out.println("funny"):
+		System.out.println("userful"):
+		System.out.println("code"):
+		System.out.println("key"):
 
 	}
 
